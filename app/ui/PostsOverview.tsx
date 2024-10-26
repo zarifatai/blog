eaa97bdfe0e832d7a8f93e1d44919e1030a3b38f
Zarif Atai <z.atai@protonmail.com>
2024-10-26 23:58:05 +0200

    Added posts overview component

--- app/ui/PostsOverview.tsx
+++ app/ui/PostsOverview.tsx
@@ -1,5 +1,46 @@
-export default function PostsOverview() {
+import fs from "node:fs/promises"
+import path from "node:path";
+import matter from "gray-matter"
+import { FrontMatter } from "@/app/types/post";
+
+export default async function PostsOverview() {
+  const frontMatters = await getFrontMatters();
+
   return (
-    <div>Post</div>
+    <div>
+      {frontMatters?.map((frontMatter) => (
+        <div>
+          <h3>{frontMatter.title}</h3>
+          <h4>{frontMatter.date}</h4>
+          <h4>{frontMatter.author}</h4>
+          <p>{frontMatter.excerpt}</p>
+        </div>
+      ))}
+    </div>
   );
+}
+
+const getFrontMatters = async () => {
+  const pathName = path.join(process.cwd(), "posts");
+  const files = await fs.readdir(pathName);
+
+  const frontMatters = await Promise.all(
+    files
+      .filter(file => file.endsWith(".md"))
+      .map(async (file) => {
+        const filePath = path.join(pathName, file);
+        const markdownWithMeta = await fs.readFile(filePath, "utf-8");
+
+        const { data: metadata } = matter(markdownWithMeta);
+        const frontMatter: FrontMatter = {
+          title: metadata.title,
+          date: metadata.date,
+          author: metadata.author,
+          excerpt: metadata.excerpt,
+        };
+
+        return frontMatter;
+      }));
+
+  return frontMatters
 }
